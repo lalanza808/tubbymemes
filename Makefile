c9bc600b3af4c4f5ec10bac946fe01090391d044
lza_menace <lza_menace@protonmail.com>
2021-12-18 01:51:42 -0800

    start fleshing out more of meme uploader

--- Makefile
+++ Makefile
@@ -11,6 +11,11 @@ dev:
 prod:
 	bash manage.sh prod
 
+install-ipfs:
+	wget https://dist.ipfs.io/go-ipfs/v0.10.0/go-ipfs_v0.10.0_linux-amd64.tar.gz
+	tar -xvzf go-ipfs_v0.10.0_linux-amd64.tar.gz
+	cd go-ipfs && bash install.sh
+
 huey:
 	mkdir -p data/
 	.venv/bin/huey_consumer suchwowx.tasks.huey -w 1 -v | tee -a data/huey.log
--- suchwowx/routes.py
+++ suchwowx/routes.py
@@ -1,8 +1,50 @@
-from flask import Blueprint
+import ipfsApi
+from os import path
+from secrets import token_urlsafe
+from json import loads, dumps
+
+from flask import Blueprint, render_template, request, current_app
 
 
 bp = Blueprint('meta', 'meta')
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    return 'interplanetary memes'
+    if "file" in request.files:
+        title = request.form.get('title')
+        description = request.form.get('description')
+        creator = request.form.get('creator')
+        file = request.files["file"]
+        filename = "{}{}".format(
+            token_urlsafe(24),
+            path.splitext(file.filename)[1]
+        )
+        file.save(filename)
+        try:
+            client = ipfsApi.Client('127.0.0.1', 5001)
+            artwork_hash = client.add(filename)['Hash']
+            client.pin_add(artwork_hash)
+            print(f'[+] Uploaded artwork to IPFS: {artwork_hash}')
+            # Create meta json
+            meta = {
+                'name': title,
+                'description': description,
+                'image': f'ipfs://{artwork_hash}',
+                'by': creator,
+                'properties': {
+                    'creator': creator
+                }
+            }
+            print(meta)
+            meta_hash = client.add_json(meta)
+            client.pin_add(meta_hash)
+            print(f'[+] Uploaded metadata to IPFS: {meta_hash}')
+        except ConnectionError:
+            print('[!] Unable to connect to local ipfs')
+        except Exception as e:
+            print(e)
+    return render_template('index.html')
+
+@bp.route('/next')
+def next():
+    return render_template('next.html')
