9df95c1f7c41e584b62033c5d4c9832e4c340390
lza_menace <lza_menace@protonmail.com>
2021-12-30 13:09:22 -0800

    trying to improve js, setting up minting

--- suchwowx/static/js/main.js
+++ suchwowx/static/js/main.js
@@ -1,12 +1,3 @@
-async function getSignedData(publicAddress, jsonData) {
-  const signedData = await window.ethereum.request({
-    method: 'eth_signTypedData_v3',
-    params: [publicAddress, JSON.stringify(jsonData)]
-  });
-  console.log(signedData);
-  return signedData
-}
-
 async function notif(s, t) {
   new Noty({
     type: t,
--- suchwowx/static/js/metamask.js
+++ suchwowx/static/js/metamask.js
@@ -2,6 +2,15 @@ window.addEventListener('DOMContentLoaded', () => {
   onboardMetaMask();
 })
 
+async function getSignedData(publicAddress, jsonData) {
+  const signedData = await window.ethereum.request({
+    method: 'eth_signTypedData_v3',
+    params: [publicAddress, JSON.stringify(jsonData)]
+  });
+  console.log(signedData);
+  return signedData
+}
+
 async function onboardMetaMask(){
   const onboarding = new MetaMaskOnboarding();
   const onboardButton = document.getElementById('metamaskConnect');
